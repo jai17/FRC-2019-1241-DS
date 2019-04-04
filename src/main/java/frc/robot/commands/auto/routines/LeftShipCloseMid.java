7530e3c39b8db0541dc13e68aa0cb22d7d5c9eff
jai17 <fire.racer_17@hotmail.com>
2019-04-04 13:34:30 -0400

    Mirroring

--- src/main/java/frc/robot/commands/auto/routines/LeftShipCloseMid.java
+++ src/main/java/frc/robot/commands/auto/routines/LeftShipCloseMid.java
@@ -8,27 +8,58 @@
 package frc.robot.commands.auto.routines;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.NumberConstants;
+import frc.robot.commands.auto.DriveDistance;
+import frc.robot.commands.auto.DriveToGoal;
+import frc.robot.commands.auto.DriveTurn;
+import frc.robot.commands.auto.EjectHatchSequence;
+import frc.robot.commands.auto.ElevatorSetpointWait;
+import frc.robot.commands.auto.SetXY;
+import frc.robot.commands.auto.YeetOffSequence;
+import frc.robot.commands.carriage.SetClawCommand;
+import frc.robot.commands.carriage.SetTrayCommand;
+import frc.robot.commands.elevator.ElevatorSetpoint;
+import frc.robot.util.FieldPoints;
 
 public class LeftShipCloseMid extends CommandGroup {
   /**
    * Add your docs here.
    */
   public LeftShipCloseMid() {
-    // Add Commands here:
-    // e.g. addSequential(new Command1());
-    // addSequential(new Command2());
-    // these will run in order.
+   
+    addParallel(new SetXY(FieldPoints.RIGHT_LEVEL_2));
+    addSequential(new YeetOffSequence());
 
-    // To run multiple commands at the same time,
-    // use addParallel()
-    // e.g. addParallel(new Command1());
-    // addSequential(new Command2());
-    // Command1 and Command2 will run in parallel.
+    //For Without Yeet
+    //addSequential(new ElevatorSetpoint(NumberConstants.ELEVATOR_LOW_HATCH_POSITION, NumberConstants.ELEVATOR_MAX_SPEED, 0.5, 1));
 
-    // A command group will require all of the subsystems that each member
-    // would require.
-    // e.g. if Command1 requires chassis, and Command2 requires arm,
-    // a CommandGroup containing them would require both the chassis and the
-    // arm.
+    //
+    addSequential(new SetXY(FieldPoints.RIGHT_OFF_PLATFORM));
+    addSequential(new DriveToGoal(FieldPoints.CLOSE_RIGHT_CARGO_PRE_SCORE, 8, 1, false));
+    addSequential(new ElevatorSetpoint(NumberConstants.ELEVATOR_LOW_HATCH_POSITION + 4, NumberConstants.ELEVATOR_MAX_SPEED, 0.3, 1));
+
+    addSequential(new DriveTurn(-90, 1, 5));
+    addSequential(new DriveTurn(1, 4, true));
+
+    addSequential(new SetTrayCommand(false)); 
+    addSequential(new DriveDistance(38, 0, 0.35, 10 + FieldPoints.CARGO_SHIP_EJECT_DIST, true)); 
+    addSequential(new EjectHatchSequence());
+  
+    addSequential(new SetXY(FieldPoints.CLOSE_RIGHT_CARGO));
+    addSequential(new DriveToGoal(FieldPoints.PRE_FEEDER_RIGHT, 5, 1, false));
+    addSequential(new SetClawCommand(true));
+    addParallel(new DriveDistance(80, 0, 0.5, 6, true));
+    addSequential(new ElevatorSetpointWait(0.5, NumberConstants.ELEVATOR_LOW_HATCH_POSITION, NumberConstants.ELEVATOR_MAX_SPEED, 0.35, 1)); 
+    addSequential(new SetClawCommand(false));
+    addSequential(new SetXY(FieldPoints.RIGHT_FEEDER));
+    addSequential(new DriveToGoal(FieldPoints.MID_RIGHT_CARGO_PRE_SCORE, 9, 0.6, true, true));
+
+    addSequential(new DriveTurn(90, 1, 5));
+    addSequential(new DriveTurn(1, 4, true));
+
+    addSequential(new SetTrayCommand(false)); 
+    addSequential(new DriveDistance(38, 0, 0.35, 10 + FieldPoints.CARGO_SHIP_EJECT_DIST, true)); 
+    addSequential(new EjectHatchSequence());
+
   }
 }
